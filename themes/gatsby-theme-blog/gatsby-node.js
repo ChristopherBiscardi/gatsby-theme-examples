1fe264ae5dd14da0aadd85f994309be62a28a7a5
Christopher Biscardi <biscarch@sketcht.com>
2018-11-21 23:07:23 -0800

    Configure webpack to compile theme
    When shipping NPM modules, they typically need to be either
    pre-compiled or the user needs to add bundler config to process the
    files. Gatsby lets us ship the bundler config *with* the theme, so
    we never need a lib-side build step.  Since we dont pre-compile the
    theme, this is how we let webpack know how to process files.

--- themes/gatsby-theme-blog/gatsby-node.js
+++ themes/gatsby-theme-blog/gatsby-node.js
@@ -71,3 +71,24 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+/**
+ * When shipping NPM modules, they typically need to be either
+ * pre-compiled or the user needs to add bundler config to process the
+ * files. Gatsby lets us ship the bundler config *with* the theme, so
+ * we never need a lib-side build step.  Since we dont pre-compile the
+ * theme, this is how we let webpack know how to process files.
+ */
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.dirname(require.resolve('gatsby-theme-blog')),
+          use: [loaders.js()],
+        },
+      ],
+    },
+  })
+}
