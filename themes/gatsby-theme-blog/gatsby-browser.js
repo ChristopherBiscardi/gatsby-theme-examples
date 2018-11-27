10246585dcac815e258f7a2805ded2574e4d16da
Christopher Biscardi <biscarch@sketcht.com>
2018-11-27 11:00:35 -0800

    minor updates because lerna in the other commit

--- themes/gatsby-theme-blog/gatsby-browser.js
+++ themes/gatsby-theme-blog/gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ThemeProvider from 'emotion-theming'
+import { ThemeProvider } from 'emotion-theming'
 import tokens from './src/components/tokens'
 
 export const wrapRootElement = ({ element }) => {
